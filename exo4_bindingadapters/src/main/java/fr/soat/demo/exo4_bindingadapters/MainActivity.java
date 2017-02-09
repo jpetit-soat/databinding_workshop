eeff63c87c8df7e26b2d6b53f44107a7e013c85a
yann_huriez <YHuriez.prestataire@voyages-sncf.com>
2017-02-09 11:24:45 +0100

    Préparation exo 4

--- exo4_bindingadapters/src/main/java/fr/soat/demo/exo4_bindingadapters/MainActivity.java
+++ exo4_bindingadapters/src/main/java/fr/soat/demo/exo4_bindingadapters/MainActivity.java
@@ -1,5 +1,6 @@
 package fr.soat.demo.exo4_bindingadapters;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,8 +9,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.view_detailled_tv_show);
 
+        DataBindingUtil.setContentView(this, R.layout.view_filter_and_detailled_movie);
 
 //        LayerDrawable stars = (LayerDrawable) filterRating.getProgressDrawable();
 //        stars.getDrawable(2).setColorFilter(Color.YELLOW, PorterDuff.Mode.SRC_ATOP);
---
+++ exo4_bindingadapters/src/main/java/fr/soat/demo/exo4_bindingadapters/viewmodel/FiltersViewModel.java
@@ -0,0 +1,127 @@
+package fr.soat.demo.exo4_bindingadapters.viewmodel;
+
+import android.content.Context;
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import android.text.TextUtils;
+
+import com.android.databinding.library.baseAdapters.BR;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.soat.demo.exo4_bindingadapters.R;
+import fr.soat.demo.moviesmodel.business.MovieSeriesBusinessService;
+import fr.soat.demo.moviesmodel.business.filters.AbstractMovieSeriesFilter;
+import fr.soat.demo.moviesmodel.business.filters.RatingFilter;
+import fr.soat.demo.moviesmodel.business.filters.SearchFilter;
+import fr.soat.demo.moviesmodel.business.filters.TypeFilter;
+import fr.soat.demo.moviesmodel.model.CulturalType;
+import fr.soat.demo.moviesmodel.model.MovieSeriesModel;
+
+/**
+ * Created by yann_huriez on 07/02/17.
+ */
+
+public class FiltersViewModel extends BaseObservable {
+
+    private Context context;
+    private MovieSeriesBusinessService movieSeriesBusinessService;
+
+    private String searchString = null;
+    private CulturalType selectedType = null;
+    private int ratingProgress = 0;
+
+    public FiltersViewModel(Context context) {
+        this.context = context;
+        movieSeriesBusinessService = new MovieSeriesBusinessService(context);
+    }
+
+    // TODO Create methods here to allow the layout to manipulate data
+
+    public String getSearchString() {
+        return searchString;
+    }
+
+    public void setSearchString(String searchString) {
+        this.searchString = searchString;
+        notifyPropertyChanged(BR.resultCount);
+    }
+
+    public int getSelectedType() {
+        return convertCulturalTypeToButtonId(selectedType);
+    }
+
+    public void setSelectedType(int buttonId) {
+        this.selectedType = convertButtonIdToCulturalType(buttonId);
+        notifyPropertyChanged(BR.resultCount);
+    }
+
+    @Bindable
+    public float getRating() {
+        return (float) ratingProgress / 2.0f;
+    }
+
+    public int getRatingProgress() {
+        return ratingProgress;
+    }
+
+    public void setRatingProgress(int ratingProgress) {
+        this.ratingProgress = ratingProgress;
+        notifyPropertyChanged(BR.rating);
+        notifyPropertyChanged(BR.resultCount);
+    }
+
+    @Bindable
+    public String getResultCount() {
+        List<AbstractMovieSeriesFilter> filters = new ArrayList<>();
+
+        // Search
+        if(!TextUtils.isEmpty(searchString)){
+            filters.add(new SearchFilter(searchString));
+        }
+
+        // Type
+        if(selectedType != null){
+            filters.add(new TypeFilter(selectedType));
+        }
+
+        // Rating
+        if(ratingProgress >= 0){
+            filters.add(new RatingFilter(ratingProgress));
+        }
+
+        List<MovieSeriesModel> filteredMovieSeries = movieSeriesBusinessService.getFilteredMovieSeries(filters);
+        String resultCount;
+        if(filteredMovieSeries.isEmpty()){
+            resultCount = context.getString(R.string.search_button_no_result);
+        } else {
+            resultCount = context.getResources().getQuantityString(R.plurals.search_button_result, filteredMovieSeries.size(), filteredMovieSeries.size());
+        }
+        return resultCount;
+    }
+
+    private int convertCulturalTypeToButtonId(CulturalType type){
+        if(type != null) {
+            switch (type) {
+                case MOVIE:
+                    return R.id.view_filter_movie_button;
+                case SERIES:
+                    return R.id.view_filter_series_button;
+            }
+        }
+        return R.id.view_filter_all_button;
+    }
+
+    private CulturalType convertButtonIdToCulturalType(int buttonId){
+        switch (buttonId){
+            case R.id.view_filter_all_button:
+                return null;
+            case R.id.view_filter_movie_button:
+                return CulturalType.MOVIE;
+            case R.id.view_filter_series_button:
+                return CulturalType.SERIES;
+        }
+        return null;
+    }
+}
